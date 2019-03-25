a07c1f19aa0e3d9cb0362702e3dd5cbb2f18b991
varsha <varsha.deora@tothenew.com>
2019-03-25 10:33:04 +0530

    initial commit

---
+++ osgi/src/main/java/com/ttn/assignment/Student.java
@@ -0,0 +1,57 @@
+package com.ttn.assignment;
+
+public class Student {
+    private String name;
+    private int id;
+    private int age;
+    private float marks;
+
+    public Student(int id, String name, int age, float marks) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getMarks() {
+        return marks;
+    }
+
+    public void setMarks(float marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                ", marks=" + marks +
+                '}';
+    }
+}
